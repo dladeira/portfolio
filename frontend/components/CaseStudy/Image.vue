f8d791f0620347c3295ddd3a16a3f4e8482e3e84
Daniel Ladeira <dladeiratx@gmail.com>
2024-06-25 13:03:55 +0200

    Added case study copy and image enlargement effect

--- frontend/components/CaseStudy/Image.vue
+++ frontend/components/CaseStudy/Image.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="csi-container">
-        <nuxt-img class="csi-img" :src="src" :alt="alt" format="webp" />
+        <nuxt-img class="csi-img" :src="src" :alt="alt" format="webp" @click="popupOpen = true" />
         <div class="csi-hint">Click to enlarge</div>
+        <div :class="popupOpen ? 'csi-popup-open' : 'csi-popup-closed'" @click="popupOpen = false">
+            <nuxt-img class="csi-popup-image" :src="src" :alt="alt" loading="lazy" />
+        </div>
     </div>
 </template>
 
@@ -30,6 +33,47 @@
     font-size: 0.75rem;
     color: $gray;
 }
+
+.csi-popup {
+    position: fixed;
+    top: 0;
+    left: 0;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    height: 100%;
+    width: 100%;
+
+    transition: all $transition 500ms;
+
+    &-open {
+        @extend .csi-popup;
+        background-color: rgba(black, 0.6);
+        backdrop-filter: blur(4px);
+    }
+
+    &-closed {
+        @extend .csi-popup;
+        background-color: rgba(black, 0);
+
+        pointer-events: none;
+    }
+
+    &-closed .csi-popup-image {
+        transform: translateY(100vh);
+    }
+}
+
+.csi-popup-image {
+    max-height: 90%;
+    max-width: 90%;
+
+    border-radius: 10px;
+
+    transition: transform $transition 500ms;
+}
 </style>
 
 <script setup>
@@ -37,4 +81,6 @@ const props = defineProps({
     src: String,
     alt: String,
 })
+
+const popupOpen = ref(false)
 </script>
