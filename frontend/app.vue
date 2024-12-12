a16736ec61859b1890c9fbbc3abae93b53613d75
dladiera <dladeiratx@gmail.com>
2024-12-12 22:12:29 +0100

    Added google tag manager?

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,11 +1,12 @@
 <template>
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PM785BGV" height="0" width="0" style="display: none; visibility: hidden"></iframe></noscript>
     <NuxtLayout>
         <NuxtPage />
     </NuxtLayout>
 </template>
 
 <script setup>
-const { t } = useI18n();
+const { t } = useI18n()
 
 useI18n().onLanguageSwitched = (oldLocale, newLocale) => {
     useHead({
@@ -14,9 +15,8 @@ useI18n().onLanguageSwitched = (oldLocale, newLocale) => {
         htmlAttrs: {
             lang: newLocale,
         },
-        script: [{ src: "https://www.googletagmanager.com/gtag/js?id=G-DGXWE1276G" }],
-    });
-};
+    })
+}
 
 useHead({
     title: `Daniel Ladeira / ${t("meta.title")}`,
@@ -24,16 +24,18 @@ useHead({
     htmlAttrs: {
         lang: useI18n().locale.value,
     },
-    script: [{ src: "https://www.googletagmanager.com/gtag/js?id=G-DGXWE1276G" }],
-});
+})
 
 onBeforeMount(() => {
-    window.dataLayer = window.dataLayer || [];
-    function gtag() {
-        dataLayer.push(arguments);
-    }
-    gtag("js", new Date());
-
-    gtag("config", "G-DGXWE1276G");
-});
+    ;(function (w, d, s, l, i) {
+        w[l] = w[l] || []
+        w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" })
+        var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s),
+            dl = l != "dataLayer" ? "&l=" + l : ""
+        j.async = true
+        j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl
+        f.parentNode.insertBefore(j, f)
+    })(window, document, "script", "dataLayer", "GTM-PM785BGV")
+})
 </script>
