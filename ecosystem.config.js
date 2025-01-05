26ceac35045a0a94df67a8b53c69652823398514
dladiera <dladeiratx@gmail.com>
2025-01-05 21:59:18 +0100

    ???

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,9 @@
-require("dotenv").config()
-
 module.exports = {
     apps: [
         {
             name: "portfolio",
             script: "npm",
             args: "start",
-            env: process.env,
         },
     ],
 }
