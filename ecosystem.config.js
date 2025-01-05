783a5e490a70fdee4623ce648ee09c887b43e543
dladiera <dladeiratx@gmail.com>
2025-01-05 21:14:58 +0100

    Added reading enviroment variables

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,14 @@
+require("dotenv").config()
+
+console.log(process.env)
+
 module.exports = {
     apps: [
         {
             name: "portfolio",
             script: "npm",
             args: "start",
+            env: process.env,
         },
     ],
 }
